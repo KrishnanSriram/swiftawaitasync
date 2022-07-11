2a42a31b6e4730986cf2427a60a31311f3be90b5
Krishnan Sriram Rama <krishnan.sriramrama@usbank.com>
2022-07-11 10:53:27 -0400

    Initial Commit

---
+++ SimpleAsyncAwait/SimpleAsyncAwaitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleAsyncAwaitApp.swift
+//  SimpleAsyncAwait
+//
+//  Created by Sriram Rama, Krishnan on 7/11/22.
+//
+
+import SwiftUI
+
+@main
+struct SimpleAsyncAwaitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
